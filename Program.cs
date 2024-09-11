7cb2dbbbbf5133b29cd1cedd33c88e34ff56b975
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-11 19:50:33 -0400

    Update Program.cs
    Main program code.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,7 @@ class Program
         var input = new ConsoleInput();
         var output = new ConsoleOutput();
 
-        // Create an instance of CharacterManager and call the Run method to start the program
-        // This will enable the user to interact with the program and manage characters
-        // The reason for creating an instance of CharacterManager is to separate the concerns of the program
-        // This will allow for easier testing and maintenance of the program
-        var manager = new CharacterManager(input, output);
+        CharacterManager manager = new CharacterManager(input, output);
         manager.Run();
     }
 }
